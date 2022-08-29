1750bc9cdd58fdb8f40eb619fd5a468eccf76616
rick <rickdev564@gmail.com>
2022-08-30 02:09:20 +0600

    9th commit

--- main.js
+++ main.js
@@ -8,15 +8,38 @@
 
 
 // })
+// perPlayer
 document.getElementById('btn-calc').addEventListener('click', function () {
     const NumberField = document.getElementById('inputNumber');
     const numberInputstring = NumberField.value;
     const numberInput = parseFloat(numberInputstring);
     const currentnumber = numberInput * 5;
-
     const price = document.getElementById('dollerInput');
     const priceElement = price.innerText;
+
+
     price.innerText = currentnumber;
     NumberField.value = '';
+
+})
+// TotalCost
+document.getElementById('btn-calcTotal').addEventListener('click', function () {
+    const ManagerField = document.getElementById('managerInput');
+    const ManagerTextString = ManagerField.value;
+    const ManagerText = parseFloat(ManagerTextString);
+    const CoachField = document.getElementById('coachInput');
+    const CoachTextString = CoachField.value;
+    const CoachText = parseFloat(CoachTextString);
+
+
+
+    const TotalCost = ManagerText + CoachText;
+
+
+    const TotalValue = document.getElementById('TotalDoller');
+    const TotalValueInner = TotalValue.innerText;
+    TotalValue.innerText = TotalCost;
+    ManagerField.value = '';
+    CoachField.value = '';
 
 })
