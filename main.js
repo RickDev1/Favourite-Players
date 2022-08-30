0cc6cc02213649335e0758d8e40e9986658a02ec
rick <rickdev564@gmail.com>
2022-08-30 15:07:51 +0600

    10th commit

--- main.js
+++ main.js
@@ -1,13 +1,62 @@
 // console.log('hello js');
 // select-V
-// document.getElementById('btn-1').addEventListener('click', function () {
-//     const playerOneField = document.getElementById('messi');
-//     const playerName = document.getElementById('player1');
-//     const playerValue = playerName.innerText;
-//     playerOneField.value = playerValue
+
+document.getElementById('btn-1').addEventListener('click', function () {
+    const PlayerField = document.getElementById('messi');
+    const playerOne = PlayerField.innerText;
+
+    const InputPlayer = document.getElementById('player1');
+    const InputPlayerOne = InputPlayer.innerText;
+    InputPlayer.innerText = playerOne;
 
 
-// })
+})
+document.getElementById('btn-2').addEventListener('click', function () {
+    const PlayerField = document.getElementById('neymar');
+    const playerOne = PlayerField.innerText;
+    const InputPlayer = document.getElementById('player2');
+    const InputPlayerOne = InputPlayer.innerText;
+    InputPlayer.innerText = playerOne;
+
+
+})
+document.getElementById('btn-3').addEventListener('click', function () {
+    const PlayerField = document.getElementById('mbappe');
+    const playerOne = PlayerField.innerText;
+    const InputPlayer = document.getElementById('player3');
+    const InputPlayerOne = InputPlayer.innerText;
+    InputPlayer.innerText = playerOne;
+
+
+})
+document.getElementById('btn-4').addEventListener('click', function () {
+    const PlayerField = document.getElementById('victor');
+    const playerOne = PlayerField.innerText;
+    const InputPlayer = document.getElementById('player4');
+    const InputPlayerOne = InputPlayer.innerText;
+    InputPlayer.innerText = playerOne;
+
+
+})
+document.getElementById('btn-5').addEventListener('click', function () {
+    const PlayerField = document.getElementById('ramos');
+    const playerOne = PlayerField.innerText;
+    const InputPlayer = document.getElementById('player5');
+    const InputPlayerOne = InputPlayer.innerText;
+    InputPlayer.innerText = playerOne;
+
+
+})
+document.getElementById('btn-6').addEventListener('click', function () {
+    const PlayerField = document.getElementById('sanches');
+    const playerOne = PlayerField.innerText;
+    const InputPlayer = document.getElementById('player5');
+    const InputPlayerOne = InputPlayer.innerText;
+    InputPlayer.innerText = playerOne;
+
+
+})
+
 // perPlayer
 document.getElementById('btn-calc').addEventListener('click', function () {
     const NumberField = document.getElementById('inputNumber');
@@ -31,9 +80,13 @@ document.getElementById('btn-calcTotal').addEventListener('click', function () {
     const CoachTextString = CoachField.value;
     const CoachText = parseFloat(CoachTextString);
 
+    const price = document.getElementById('dollerInput');
+
+    const priceElementstring = price.innerText;
+    const priceElement = parseFloat(priceElementstring);
 
 
-    const TotalCost = ManagerText + CoachText;
+    const TotalCost = priceElement + ManagerText + CoachText;
 
 
     const TotalValue = document.getElementById('TotalDoller');
